330717f30316cc410da9fad077b48471a68dd5e2
MonikaLaur <monika.laurinaviciute01@gmail.com>
2017-11-27 18:11:51 +0100

    commit 4

---
+++ Class7/Example.py
@@ -0,0 +1,8 @@
+print "Hello World!" # strings
+print 3  # integer
+print 4.34 # float
+print True # bool
+print 3+4 # 7
+print True and False # False
+print True or False # True
+print True and not False # True 
