572bcfd5fa4819c2c66c14dab051f9924d5a591a
MonikaLaur <monika.laurinaviciute01@gmail.com>
2017-12-06 17:07:43 +0100

    Commit 7

--- Class8/Excercise 8.1.py
+++ Class8/Excercise 8.1.py
@@ -2,9 +2,14 @@ print "Welcome to Unit Converter!"
 print "here you can convert Kilometers into Miles"
 print "="*70
 
-km = raw_input("Please enter kilometer value:  ")
-km = float(km)
+while True:
+    km = raw_input("Please enter kilometer value:  ")
+    km = float(km)
 
-total = km*0.621371
+    total = km*0.621371
 
-print ("%.4f" % total)+ "  Miles (rounded up to 4 decimal points)"
+    print ("%.4f" % total)+ "  Miles (rounded up to 4 decimal points)"
+
+    entry = raw_input("Would you like to convert another value? (press n to exit)")
+    if entry == "n":
+        break
